953b57035c05270cb494f98019d2d6fd96706abd
penguinone <penguinone2580@gmail.com>
2020-02-24 02:31:06 +0900

    bug fix

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from flask import Flask, request, abort
-import urllib
-import urllib.request
-import json
-from datetime import datetime
 import re
 
 from linebot import (
@@ -15,6 +11,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, MessageAction, ButtonsTemplate,
 )
 import os
+
+import Func
 
 app = Flask(__name__)
 
@@ -69,34 +67,6 @@ def handle_message(event):
                                                         title='リーグマッチ募集',
                                                         text="a",
                                                         actions=[MessageAction(label='参加する', text='参加')])))
-
-def get_rule_stage(hour):
-    req = urllib.request.Request("https://spla2.yuu26.com/{rule}/schedule".format(rule='league'))
-    req.add_header("user-agent", "@penguinone2580")
-    with urllib.request.urlopen(req) as res:
-        response_body = res.read().decode("utf-8")
-        response_json = json.loads(response_body.split("\n")[0])
-        data = response_json["result"]
-        for d in data:
-            start_time = datetime.strptime(d["start"], '%Y-%m-%dT%H:%M:%S')
-            end_time = datetime.strptime(d["end"], '%Y-%m-%dT%H:%M:%S')
-            start_hour = start_time.strftime("%H")
-            if hour == int(start_hour):
-                text = "{start} ~ {end}\n{rule}\n{stage1}\t{stage2}".format(
-                    start=start_time.strftime("%m/%d %H:%M"),
-                    end=end_time.strftime("%m/%d %H:%M"),
-                    rule=d["rule_ex"]["name"],
-                    stage1=d["maps_ex"][0]["name"],
-                    stage2=d["maps_ex"][1]["name"])
-
-                return text
-
-def check_hour(hour):
-    if hour==0:
-        hour = 23
-    elif hour%2==0:
-        hour = hour - 1
-    return hour
 
 if __name__ == "__main__":
 #    app.run()
